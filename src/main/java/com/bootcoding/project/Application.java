f4a4e19238e57c8d0df51e55ec44964159255a8d
bootcoding-developer <bootcoding.dev@gmail.com>
2023-09-07 16:22:04 +0530

    model classes created

---
+++ src/main/java/com/bootcoding/project/Application.java
@@ -0,0 +1,9 @@
+package com.bootcoding.project;
+
+import com.bootcoding.project.model.Customer;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Project!");
+    }
+}
---
+++ src/main/java/com/bootcoding/project/model/OrderMenuItem.java
@@ -0,0 +1,8 @@
+package com.bootcoding.project.model;
+
+public class OrderMenuItem {
+    int id;
+    int numberOfMenus;
+    double totalPrice;
+
+}
