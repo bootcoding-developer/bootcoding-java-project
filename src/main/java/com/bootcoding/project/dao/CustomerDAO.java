230dc7a7cd9a79d27d4da14f7cfae2e201c54113
bootcoding-developer <bootcoding.dev@gmail.com>
2023-09-09 12:42:24 +0530

    SELECT Query for Customer

--- src/main/java/com/bootcoding/project/dao/CustomerDAO.java
+++ src/main/java/com/bootcoding/project/dao/CustomerDAO.java
@@ -2,18 +2,55 @@ package com.bootcoding.project.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class CustomerDAO {
 
+
     public static void main(String[] args) {
         CustomerDAO cd = new CustomerDAO();
-        for(int i = 103; i < 10000; i++){
+        for(int i = 103; i < 1000; i++){
             cd.insertCustomer(i);
             System.out.println("New record Inserted into database!");
         }
+        cd.getCustomerData();
     }
 
+    public void getCustomerData(){
+        // 1. Load JDBC Driver into Memory
+        // 2. Create Database Connection
+        // 3. Create Statement to write a SQL Query
+        // 4. Get Result into ResultSet
+        // 5. Close DB Connection
+        try{
+            Class.forName("org.postgresql.Driver");
+
+            // 2. Create Database Connection
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                    "postgres", "postgres");
+            // 3. Create Statement to write a SQL Query
+            Statement stmt = connection.createStatement();
+            // 4. Get Result into ResultSet
+            ResultSet resultSet = stmt.executeQuery("SELECT * FROM CUSTOMER");
+
+            //Extract Data from ResultSet
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String address = resultSet.getString("address");
+                System.out.println("ID: " + id + ", " +
+                        "Name: "+ name + ", Addr:"+ address);
+
+            }
+
+            // 5. Close DB Connection
+            connection.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
     public void insertCustomer(int i){
 
         // Database Connectivity in Java
@@ -28,7 +65,7 @@ public class CustomerDAO {
             Class.forName("org.postgresql.Driver");
 
             // 2. Create Database Connection
-           Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
                     "postgres", "postgres");
             // 3. Create Statement to write a SQL Query
             Statement stmt = connection.createStatement();
